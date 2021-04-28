a968de32dc1a98964277ddd66d195bebb700ae49
Dchole <derekoware47@gmail.com>
2021-04-28 09:07:26 +0000

    update table model

--- client/src/db/tables.ts
+++ client/src/db/tables.ts
@@ -1,15 +1,21 @@
-import { IValues } from "../components/CreateTableForm/Reducer";
+import { initialState, IValues } from "../components/CreateTableForm/Reducer";
 import Db from "./init";
 
 class Table extends Db {
   private collection = this.database.collection("tables");
+  private data = {};
+
+  constructor() {
+    super();
+    this.data = { ...initialState, rows: [], admin: "" };
+  }
 
   public get tables() {
     return this.collection.get();
   }
 
   async createTable(values: IValues) {
-    return this.collection.add(values);
+    return this.collection.add({ ...this.data, ...values });
   }
 }
 
